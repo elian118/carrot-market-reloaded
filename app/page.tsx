fe0c5a08e1a6815ed1cd72c47b37b2a202fe102f
elian <elian118@naver.com>
2024-03-20 17:45:17 +0900

    3.5 Responsive Modifier

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,26 @@
 export default function Home() {
   return (
-    <main className="bg-gray-100 h-screen flex items-center justify-center p-5">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-2">
+    <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2">
         <input
           className="w-full rounded-full h-10 bg-gray-200 py-6 pl-5 outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow"
           type="text"
           placeholder="Search here..."
         />
-        <button className="bg-black bg-opacity-50 text-white py-2 rounded-full active:scale-90 transition-transform font-medium focus:scale-90 outline-none">
+        <button className="bg-black bg-opacity-50 text-white py-2 rounded-full active:scale-90 transition-transform font-medium focus:scale-90 outline-none md:px-8">
           Search
         </button>
       </div>
     </main>
   );
 }
+
+/*
+  tailwind css 반응형 수정자의 경우, 기본 가장 작은 사이즈부터 스타일을 적용하도록 설계돼 있다.
+
+    <main className="bg-gray-100 sm:bg-red-100" />{children}</main
+
+  위 코드에서 main 은 배경색이 회색100에서 가로 640px이 되는 순간 빨강100으로 변경된 이후부터 계속 빨강 100이 유지된다.
+  md, lg, xl과 같은 더 큰 사이즈가 적용됐을 때 추가 테일윈드 유틸리티 클래스 수정자가 여기서는 존재하지 않기 때문이다.
+  
+*/
