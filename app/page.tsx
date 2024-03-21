f99bd9f030be8e7016b7b4f993c6e32d3464429c
elian <elian118@naver.com>
2024-03-21 10:08:35 +0900

    3.7 State Modifiers

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 export default function Home() {
   return (
     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2 md:items-start">
-        <div className="flex flex-col md:w-full">
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2 md:items-start ring ring-transparent transition-shadow has-[:invalid]:ring-red-100 has-[:invalid]:ring">
+        <div className="flex flex-col md:w-full *:outline-none">
           <input
-            className="w-full rounded-full h-10 bg-gray-200 py-6 pl-5 outline-none ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
-            type="email"
+            className="w-full rounded-full h-10 bg-gray-200 py-6 pl-5 ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
+            type="text"
             placeholder="Email address"
             required // peer-required 연동
           />
@@ -13,7 +13,10 @@ export default function Home() {
             Email is required.
           </span>
         </div>
-        <button className="bg-black bg-opacity-50 text-white py-2 md:w-1/4 rounded-full active:scale-90 transition-transform font-medium focus:scale-90 outline-none md:px-8 md:h-12">
+        <button
+          id="logInBtn"
+          className="bg-black bg-opacity-50 text-white py-2 md:w-1/4 rounded-full active:scale-90 transition-transform font-medium focus:scale-90 md:px-8 md:h-12"
+        >
           Log in
         </button>
       </div>
@@ -40,4 +43,22 @@ export default function Home() {
 
     형제의 peer 가 지정된 폼 요소 속성 변경을 감지해 peer- 접두어를 같은 형제에 사용함으로써 조건부 스타일을 활성화하는 수정자
     같은 형제 요소끼리만 적용 가능 - peer 영역을 벗어나면 peer-[className] 효과가 발동하지 않는다.
+
+  4. *:[className]
+
+    *:outline-none
+
+    자식 요소 전부 공통 스타일을 적용하고자 할 때 부모 요소에서 걸 수 있는 수정자
+    자식만 될 뿐, 손자 이상 깊은 스타일의 상속은 불가하다.
+
+  5. has-[.className | #idName | :modifier | ...]:
+
+    has-[.peer]:bg-green-100
+    has-[#logInBtn]:bg-green-100
+    has-[:invalid]:bg-red-100
+
+    자식 요소들 중 특정 식별자 또는 테일윈드 수정자 등을 포함하는 요소가 하나라도 있다면 지정한 스타일을 적용한다.
+    손자 이상 깊은 요소까지 감별하는 듯하다.
+
+    이러한 동적 스타일 적용은 최근 업데이트 된 강력한 CSS 고유 기능으로 제공되며 가상 클래스 기반으로 작동한다.
 */
