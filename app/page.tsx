b575c3293e9b04a4e0656c200c1fdc91a0854edf
elian <elian118@naver.com>
2024-03-21 10:08:35 +0900

    3.7 State Modifiers

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2 md:items-start ring ring-transparent transition-shadow has-[:invalid]:ring-red-100 has-[:invalid]:ring">
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2 md:items-start ring ring-transparent transition-shadow has-[:invalid]:ring-red-100">
         <div className="flex flex-col md:w-full *:outline-none">
           <input
             className="w-full rounded-full h-10 bg-gray-200 py-6 pl-5 ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
